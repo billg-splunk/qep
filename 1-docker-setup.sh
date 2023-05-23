96a60bc51b92f13bb90a2ff036afb60412d5d76d
billg-splunk <billg@splunk.com>
2023-05-23 10:58:00 -0400

    minor change to install

--- 1-docker-setup.sh
+++ 1-docker-setup.sh
@@ -14,7 +14,7 @@ sudo chmod +r /etc/rancher/k3s/k3s.yaml
 
 # (2) Install docker
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install -y ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
