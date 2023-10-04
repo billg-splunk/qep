0b9cdb626e915fe1e6128559a0a87ce907e4a745
billg-splunk <billg@splunk.com>
2023-10-04 17:37:25 -0400

    otel now removed/installed vs. upgraded

--- 2-remove-diab.sh
+++ 2-remove-diab.sh
@@ -3,6 +3,7 @@
 # This setup script will:
 # (1) Remove demoinabox demos
 # (2) Stop the demoinabox frontend
+# (3) Remove the OTelCollector (or multiple, if found)
 
 # General variables
 SCRIPT=$(readlink -f "$0")
@@ -17,7 +18,18 @@ kubectl delete -f ~/demo-in-a-box*/hack/yaml/demoinabox-backend.yaml
 # (2) Stop the demoinabox frontend
 sudo systemctl stop demoinabox.service
 
+# (3) Remove any otel collectors
+helmlist=$(helm list)
+re="(splunk-otel-collector[^[:space:]]+)"
+while [[ $helmlist =~ $re ]]; do
+  HELMRELEASE=${BASH_REMATCH[1]};
+  # Remove otel collector
+  echo "INFO: Removed otel collector $HELMRELEASE"
+  helm uninstall $HELMRELEASE
+  helmlist=$(helm list)
+done
+
 echo ""
 echo ""
 echo ""
-echo DemoInABox has been removed.
+echo DemoInABox and the OTel Collector has been removed.
--- 3-upgrade-otel-and-deploy-app.sh
+++ 3-upgrade-otel-and-deploy-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This setup script will:
-# (1) Upgrade the otel collector (for your org)
+# (1) Install the otel collector (for your org)
 # (2) Deploy the hipster shop app
 
 # General variables
@@ -18,7 +18,7 @@ read INGEST_TOKEN
 echo 'Enter rum token:'
 read RUM_TOKEN
 
-# (1) Upgrade the otel collector (for your org)
+# (1) Install the otel collector (for your org)
 OTEL_VALUES_PATH="$SCRIPTPATH/otel/values.yaml"
 MY_OTEL_VALUES_PATH="$SCRIPTPATH/otel/values-mine.yaml"
 # Remove if previous exists
@@ -30,21 +30,13 @@ cp $OTEL_VALUES_PATH $MY_OTEL_VALUES_PATH
 # Update environment
 sed -i "s/{{environment}}/$ENVIRONMENT/" $MY_OTEL_VALUES_PATH
 
-# Get the helm release
-helmlist=$(helm list)
-re="(splunk-otel-collector[^[:space:]]+)"
-if [[ $helmlist =~ $re ]]; then
-  HELMRELEASE=${BASH_REMATCH[1]};
-  # Upgrade otel collector
-  helm upgrade --set cloudProvider=" " --set distribution=" " \
-  --set splunkObservability.accessToken="$INGEST_TOKEN" \
-  --set clusterName="$ENVIRONMENT" --set splunkObservability.realm="$REALM" \
-  --set gateway.enabled="false" \
-  -f $MY_OTEL_VALUES_PATH \
-  $HELMRELEASE splunk-otel-collector-chart/splunk-otel-collector
-else
-  echo "ERROR: Cannot find helm release to upgrade the otel collector.";
-fi
+# Install the otel collector
+helm install --set cloudProvider=" " --set distribution=" " \
+--set splunkObservability.accessToken="$INGEST_TOKEN" \
+--set clusterName="$ENVIRONMENT" --set splunkObservability.realm="$REALM" \
+--set gateway.enabled="false" \
+-f $MY_OTEL_VALUES_PATH \
+--generate-name splunk-otel-collector-chart/splunk-otel-collector 
 
 # (2) Deploy the hipster shop app
 
@@ -68,7 +60,7 @@ kubectl apply -f $MY_HIPSTERSHOP_PATH
 echo ""
 echo ""
 echo ""
-echo Upgraded the otel collector for your environment.
+echo Installed the otel collector for your environment.
 echo Deployed the base hipster shop application.
 echo ""
 echo Check in O11y Cloud if you can find the application
